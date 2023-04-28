b544171d866e0efc1fd4d683d91ef0e01815b578
Uma <umaram2694@gmail.com>
2023-04-28 18:15:36 +0530

    Commit_5

--- cypress.config.js
+++ cypress.config.js
@@ -2,12 +2,12 @@ const { defineConfig } = require("cypress");
 const { cloudPlugin } = require("cypress-cloud/plugin");
 module.exports = defineConfig({
   e2e: {
-    setupNodeEvents(on:any, config:any) {
+    setupNodeEvents(on, config) {
       // implement node event listeners here
       return cloudPlugin(on, config);
     },
     specPattern: 'cypress/e2e/**.{js,jsx,ts,tsx}',
-        projectId: "Dashboard_Test",
+        projectId: "Demo_Project",
         experimentalRunAllSpecs: true
     
   },
